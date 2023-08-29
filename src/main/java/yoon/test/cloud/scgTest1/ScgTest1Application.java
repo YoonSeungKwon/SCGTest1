395320b15a186871a6748d04b1babf33ba86fceb
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-08-29 23:06:57 +0900

    //

---
+++ src/main/java/yoon/test/cloud/scgTest1/ScgTest1Application.java
@@ -0,0 +1,13 @@
+package yoon.test.cloud.scgTest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/yoon/test/cloud/scgTest1/ScgTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package yoon.test.cloud.scgTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
